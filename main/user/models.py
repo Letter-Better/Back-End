f7f5d175a6de13c73eebf2beb1a308434eff2413
MahanBi <DirtyMBGamer@gmail.com>
2021-12-07 02:39:07 +0330

    Status|model

--- main/user/models.py
+++ main/user/models.py
@@ -48,3 +48,18 @@ class Friend(models.Model):
     def __str__(self) -> str:
         return f"{self.user} {self.friend}"
 
+class Status(models.Model):
+    user = models.OneToOneField(User, db_column="user", related_name="user_status_set", on_delete=models.CASCADE)
+    total = models.IntegerField("Total", db_column="total", default=0)
+    win = models.IntegerField("Win", db_column="win", default=0)
+    lose = models.IntegerField("Lose", db_column="lose", default=0)
+    level = models.IntegerField("level", db_column="level", default=0)
+    point = models.IntegerField("point", db_column="point", default=0)
+    create_at = models.DateTimeField("Created At", db_column="create_at", auto_now_add=True, editable=False)
+    update_at = models.DateTimeField("Update At", db_column="update_at", auto_now=True)
+
+    class Meta:
+        db_table = "Status"
+
+    def __str__(self) -> str:
+        return self.user
