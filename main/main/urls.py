1816879de35098285c46287a5aea72b261d5753b
MahanBi <DirtyMBGamer@gmail.com>
2021-12-09 21:57:51 +0330

    fix maim-urls

--- main/main/urls.py
+++ main/main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('user/login/', views.obtain_auth_token, name='auth_token'),
-    path('user/', include('user.urls', namespace='user'))
+    path('user/', include('user.urls', namespace='user')),
 ]
