89a366207b60c618def49eb399a731d6962a2f87
MahanBi <DirtyMBGamer@gmail.com>
2021-12-14 10:26:31 +0330

    add default throttle

--- main/main/settings.py
+++ main/main/settings.py
@@ -37,7 +37,15 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'    
-    ]
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
 }
 
 INSTALLED_APPS = [
