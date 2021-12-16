f7841023b11c278f027af726a23e7d2d77c45970
MahanBi <DirtyMBGamer@gmail.com>
2021-12-16 14:09:44 +0330

    add mission and User Ranking

--- main/user/signals.py
+++ main/user/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-from .models import User, Status
+from .models import User, Status, UserMission
 
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
@@ -12,3 +12,8 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
 def create_status(sender, instance=None, created=False, **kwargs):
     if created:
         Status.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def create_user_mission(sender, instance=None, created=False, **kwargs):
+    if created:
+        UserMission.objects.create(user=instance)
