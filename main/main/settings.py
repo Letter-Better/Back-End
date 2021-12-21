04a0939136185d19bc485842f2c501f33277760b
MahanBi <DirtyMBGamer@gmail.com>
2021-12-21 23:42:28 +0330

    change auth from token to jwt

--- main/main/settings.py
+++ main/main/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import timedelta
 import json
 import redis
 
@@ -43,12 +44,43 @@ CHANNEL_LAYERS = {
     },
 }
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': conf["jwt"]["ALGORITHM"],
+    'SIGNING_KEY': conf["settings"]["SECRET_KEY"],
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'    
+        'rest_framework_simplejwt.authentication.JWTAuthentication'    
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
@@ -75,6 +107,7 @@ INSTALLED_APPS = [
     'campaign.apps.CampaignConfig',
     # 3rd Apps
     'rest_framework',
+    'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'channels',
 ]
