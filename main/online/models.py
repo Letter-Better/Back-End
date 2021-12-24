d26adcf85d2859f9eb560c949a39e12aff12ebe6
MahanBi <DirtyMBGamer@gmail.com>
2021-12-24 19:42:34 +0330

    online models

--- main/online/models.py
+++ main/online/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+class categorty(models.Model):
+    name = models.CharField()
+    slug = models.SlugField()
+
+
+class word(models.Model):
+    DIFFICULTY = (
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard'),
+    )
+
+    word = models.CharField()
+    lenght = models.IntegerField()
+    category = models.ForeignKey(categorty)
+    difficulty = models.CharField(verbose_name="Difficulty", db_column="difficulty", choices=DIFFICULTY, default='easy', max_length=50, error_messages=MESSAGES)
+
+
--- main/user/authentications.py
+++ main/user/authentications.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.backends import BaseBackend
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
