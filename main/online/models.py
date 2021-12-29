3e0c61b1eb82cec98c5014be24e10cd226a83e24
MahanBi <DirtyMBGamer@gmail.com>
2021-12-29 17:51:33 +0330

    complete Room model

--- main/online/models.py
+++ main/online/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from user.models import User
 from django.utils.translation import gettext_lazy as _
 import uuid
+
 MESSAGES = {
     "unique": "...",
     "required": "...",
@@ -13,7 +14,7 @@ MESSAGES = {
 
 class Room(models.Model):
 
-    def random_friend_id() -> str: return uuid.uuid4().hex[:20].lower()
+    def random_room_code() -> str: return uuid.uuid4().hex[:20].lower()
 
     DIFFICULTY = (
         ('easy', 'Easy'),
@@ -30,11 +31,23 @@ class Room(models.Model):
         Seven = 7, _('Seven')
         Eight = 8, _('Eight')
 
-    creator = models.OneToOneField(User, on_delete=models.CASCADE)
-    members = models.ForeignKey(User, on_delete=models.CASCADE)
-    difficulty = models.CharField(verbose_name="Difficulty", db_column="difficulty", choices=DIFFICULTY, default='easy', max_length=50, error_messages=MESSAGES)
+    class Round(models.IntegerChoices):
+        Two = 2, _('Two')
+        Three = 3, _('Three')
+        Four = 4, _('Four')
+
+    class Time(models.IntegerChoices):
+        ten_sec = 10, _('Ten Sec')
+        twenty_sec = 20, _('Twenty Sec')
+        thirty_sec = 30, _('Thirty Sec')
+
+    creator = models.OneToOneField(User, verbose_name="Creator", db_column="creator", on_delete=models.CASCADE, error_messages=MESSAGES)
+    members = models.ForeignKey(User, verbose_name="Members", db_column="memebrs", on_delete=models.CASCADE, error_messages=MESSAGES)
+    time_of_draw = models.IntegerField(verbose_name="Time", db_column="time", choices=Time.choices, default=Time.thirty_sec, error_messages=MESSAGES)
+    round = models.IntegerField(verbose_name="round", db_column="round", choices=Round.choices, default=Round.Two, error_messages=MESSAGES)
     number_of_users = models.IntegerField(verbose_name="number of user", db_column="number_of_users", choices=NumberOFUsers.choices, default=NumberOFUsers.Three, error_messages=MESSAGES)
-    room_code = models.CharField(verbose_name="Room Code", db_column="friend_code", max_length=20, unique=True, default=random_friend_id, error_messages=MESSAGES)
+    difficulty = models.CharField(verbose_name="Difficulty", db_column="difficulty", choices=DIFFICULTY, default='easy', max_length=50, error_messages=MESSAGES)
+    room_code = models.CharField(verbose_name="Room Code", db_column="room_code", max_length=30, unique=True, default=random_room_code, error_messages=MESSAGES)
     create_at = models.DateTimeField(verbose_name="Created At", db_column="create_at", auto_now_add=True, editable=False, error_messages=MESSAGES)
     update_at = models.DateTimeField(verbose_name="Update At", db_column="update_at", auto_now=True, error_messages=MESSAGES)
 
@@ -73,7 +86,8 @@ class OnlineRanking(models.Model):
     update_at = models.DateTimeField(verbose_name="Update At", db_column="update_at", auto_now=True, error_messages=MESSAGES)
 
     class Meta:
-        ...
+        verbose_name_plural = "OnlineRanking"
+        db_table = "OnlineRanking"
 
     @property
     def rank(self):
