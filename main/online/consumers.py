faf3f2947b70387e03011a879b7856be59f9e34f
MahanBi <DirtyMBGamer@gmail.com>
2022-01-04 21:42:41 +0330

    TODO: online(websocket_receive)

--- main/online/consumers.py
+++ main/online/consumers.py
@@ -1,37 +1,34 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
+from channels.exceptions import StopConsumer
 from .models import Room, RoomMember
 
 class OnlineGameConsumer(AsyncJsonWebsocketConsumer):
     async def websocket_connect(self, event):
         self.room_code = self.scope["url_route"]["kwargs"]["room_code"]
         self.user = self.scope["user"]
-        print(self.user)
+        
         await self.channel_layer.group_add(
             self.room_code,
             self.channel_name
         )
-        
+
         try:
             room = database_sync_to_async(Room.objects.get(room_code=self.room_code))
         except Room.DoesNotExist:
-            print("error room_code")
+            self.close()
 
-        
-        if database_sync_to_async(RoomMember.objects.filter(room_id=room.id, members=1).exists()):
-            print("yay")
-        await self.accept()
-
-        
-
+        if database_sync_to_async(RoomMember.objects.filter(room_id=room.id, members=self.user).exists()):
+            await self.accept()
+        else: self.close()
         
     async def websocket_disconnect(self, event):
-        ...
+        await self.channel_layer.group_discard(
+            self.room_code, self.channel_name
+        )
+        raise StopConsumer()
 
     async def websocket_receive(self, message):
-        ...
+        text_data = message.get('text', None)
+        bytes_data = message.get('bytes', None)
          
-
-"""
-
-"""
